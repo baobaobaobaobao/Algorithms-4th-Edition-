dbce31386de6346c45a65147b0affdee76c09ca8
bzq <530420396@qq.com>
2018-07-20 18:14:56 +0800

    companyAlgorithm1

---
+++ algorithm/src/algorithm_1_1/EX20.java
@@ -0,0 +1,14 @@
+package algorithm_1_1;
+
+public class EX20 {
+	
+	public int  getln(int n) {
+		while(n!=0) {
+			
+		}
+		return 0;
+	}
+public static void main(String[] args) {
+	
+}
+}
--- algorithm/src/algorithm_1_1/Ex19.java
+++ algorithm/src/algorithm_1_1/Ex19.java
@@ -1,14 +1,33 @@
 package algorithm_1_1;
+
+import edu.princeton.cs.algs4.StdOut;
+
 /*没想出来*/
 public class Ex19 {
-	public static long F(int N) {
-		if(N==0) return 0;
-		if(N==1) return 1;
-		return F(N-1)+F(N-2);
+	public static long[] F(int N) {
+		long[] fibonacci = new long[N + 1];
+		if (N == 0) {
+			return fibonacci;
+		}
+		fibonacci[1] = 1;
+		if (N == 1) {
+			return fibonacci;
+		}
+		
+		/*不用递归简单的一塌糊涂啊，就是用有个关系
+		 * F(n)=F(n-1)+F(n-2)
+		 * 所以你用数组每次加入就行
+		 * */
+		for (int i = 2; i <= N; i++) {
+			fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+		}
+		return fibonacci;
 	}
+
 	public static void main(String[] args) {
-		for(int N=0;N<100;N++) {
-			System.out.println(N+"  "+F(N));
+		long[] fibonacci = F(99);
+		for (int N = 0; N < fibonacci.length; N++) {
+			StdOut.println(N + " " + fibonacci[N]);
 		}
 	}
 }
